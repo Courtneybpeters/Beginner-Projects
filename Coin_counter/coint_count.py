2f4d422d66a6bafb585b760b7d532f9749708fa1
Courtney Peters <courtneybpeters@gmail.com>
2015-04-15 17:07:45 -0500

    Coin Counter
    Going to restart coin counter

---
+++ Coin_counter/coint_count.py
@@ -0,0 +1,68 @@
+# # Esimates coin total and amount needed to wrap determined by coin weight
+#
+# class coin(object):
+#     def __init__(self, desc, value, weight, wrap_qty):
+#         self.desc = desc
+#         self.value = value
+#         self.weight = weight
+#         self.wrap_qty = wrap_qty
+#
+#     # TODO: Quantity of coins based off total weight method
+#
+#     # TODO: How many wrappers for each coin method
+#
+#
+#
+# # Values
+# VALUES = {'PENNY': .01, 'NICKEL': .05, 'DIME': .1, 'QUARTER': .5}
+#
+# # Coin weights (in grams)
+# PENNY_WEIGHT = 2.5
+# NICKEL_WEIGHT = 5.0
+# DIME_WEIGHT = 2.268
+# QUARTER_WEIGHT = 5.67
+#
+# # Coins to one wrapper
+# PENNY_WRAP = 50
+# NICKEL_WRAP = 40
+# DIME_WRAP = 50
+# QUARTER_WRAP = 40
+#
+# def main():
+#
+#     # TODO: Function that totals all the coin objects
+#
+#
+#     coins = {'PENNY': 0, 'NICKEL': 0, 'DIME': 0, 'QUARTER': 0}
+#
+#     for coin, value in coins.iteritems():
+#         print "Weight of ", coin, ": "
+#         weight = float(raw_input())
+#         coins[coin] = weight
+#
+#     print coins
+#
+#     wrappers(coins)
+#
+# '''Prints how many wrappers you will need for the amounts of coins'''
+# def wrappers(coins):
+#
+#     for coin, weight in coins.iteritems():
+#         print coin
+#
+# def get_value(coins):
+#
+#     for coin in coins.iterkeys():
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# if __name__ == "__main__":
+#     main()
